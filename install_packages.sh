9876da84a16c3a86d6446eb42ed8160bf4d411b6
szenadam <szenadam1@gmail.com>
2019-03-13 13:21:25 +0100

    Add Nodejs, eslint, docker, general packages, settings

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,11 @@ apt-get update
 apt-get upgrade
 
 # General packages
-apt-get install build-essential curl vim tree git tmux
+apt-get install -y \
+    build-essential curl vim \
+    tree git tmux gnome-tweak-tool \
+    apt-transport-https ca-certificates software-properties-common
+echo 'set -g default-terminal "screen-256color"' >> $HOME/-tmux.conf
 
 # VS Code
 wget https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O vscode_stable.deb
@@ -42,6 +46,10 @@ cat << EOF > $HOME/.config/Code/User/keybindings.json
     {
         "key": "ctrl+t",
         "command": "-workbench.action.showAllSymbols"
+    },
+    {
+        "key": "ctrl+shift+numpad_add",
+        "command": "git.stageAll"
     }
 ]
 EOF
@@ -54,3 +62,26 @@ rm -f google-chrome-stable_current_amd64.deb
 
 # Spotify
 snap install spotify
+
+# Nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install nodejs
+
+# NPM packages
+npm i -g eslint
+
+# Docker
+apt update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+usermod -aG docker ${USER}
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Bash aliases
+cat <<< EOF >>> $HOME/.bashrc
+alias gs="git status"
+alias gs="git status"
